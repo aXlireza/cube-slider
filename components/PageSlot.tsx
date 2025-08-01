8e5e59290bfb7f0b1db8e3d5a6fd3ee047a9e279
aXlireza <alireza.ataee01@gmail.com>
2025-08-01 19:41:48 +0330

    feat: mount pages on cube faces

---
+++ components/PageSlot.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { ReactNode, useEffect } from 'react';
+import { useCube } from './CubeProvider';
+
+export default function PageSlot({ children }: { children: ReactNode }) {
+  const { cubeRef } = useCube();
+
+  useEffect(() => {
+    const cube = cubeRef.current;
+    if (!cube) return;
+    cube.setFaceContent('front', { content: children });
+    void cube.rotateToFace('front');
+  }, [cubeRef, children]);
+
+  return null;
+}
