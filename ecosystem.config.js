4754fd8aab95c6c8e2cc7668666936a3b8a65dd7
aXlireza <alireza.ataee01@gmail.com>
2025-08-09 14:18:29 +0330

    fixing the port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-      apps: [
-        {
-          name: 'kno-cube',
-          script: 'npm run start -p 3231',
-////.//          args: 'dev',
-//          max_memory_restart: '700M',
-//          node_args: '--max-old-space-size=600', // Adjust heap allocation
-        },
-      ],
+  apps: [
+    {
+      name: 'kno-cube',
+      script: 'npm',
+      args: 'run start -- -p 3231',
+    },
+  ],
 };
