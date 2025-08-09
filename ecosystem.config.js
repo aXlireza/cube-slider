ed7428813d02a60530f432b5309a7dd40eb5b946
aXlireza <alireza.ataee01@gmail.com>
2025-08-09 14:15:35 +0330

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+      apps: [
+        {
+          name: 'kno-cube',
+          script: 'npm run start -p 3231',
+////.//          args: 'dev',
+//          max_memory_restart: '700M',
+//          node_args: '--max-old-space-size=600', // Adjust heap allocation
+        },
+      ],
+};
